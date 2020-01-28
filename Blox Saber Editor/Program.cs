4030cf1941ccf27788bf4c528cdd89c92365a36f
haawwkeye <30607615+haawwkeye@users.noreply.github.com>
2020-01-28 11:24:52 -0500

    Everything + colorsequence.cs edit

---
+++ Blox Saber Editor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Sound_Space_Editor
+{
+	class Program
+	{
+		//TODO - add arrow controls to navigate through ticks, add an option to disable snapping to ticks
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			EditorWindow w;
+
+			try
+			{
+				long offset = 0;
+
+				if (args.Length >= 2 && args[0] == "-o")
+				{
+					long.TryParse(args[1], out offset);
+				}
+
+				w = new EditorWindow(offset);
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			using (w)
+			{
+				w.Run();
+			}
+		}
+	}
+}
